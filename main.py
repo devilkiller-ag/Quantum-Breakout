a9a3b18139bc507914908680416606394a533081
Ashmit JaiSarita Gupta <ashmitgupta.official@gmail.com>
2023-04-15 10:51:59 +0530

    Added BGM and sample video

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Import files and libraries
 import pygame
+from pygame.locals import *
+from pygame import mixer
 from assets import globals, scene
 
 # Initialise pygame and create window
@@ -7,6 +9,11 @@ pygame.init()
 screen = pygame.display.set_mode((globals.WINDOW_WIDTH, globals.WINDOW_HEIGHT))
 pygame.display.set_caption('Quantum Breakout')
 clock = pygame.time.Clock()
+
+mixer.init()
+mixer.music.load('./assets/8BitAdventure.ogg')
+mixer.music.set_volume(0.9)
+mixer.music.play()
 
 def main():
     # initialize game
